bb7f70e5e9c5429265876d754536460f0b435027
Red <crabmarket56@gmail.com>
2022-07-22 19:04:17 -0400

    Add merge method

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,9 +1,11 @@
 def merge_sort(array)
   left_side = []
   right_side = []
+  new_array = []
   i = 0
   if array.length < 2
     return
+
   else
     array.each_with_index do |element, index|
       if index < array.length/2 
@@ -16,9 +18,32 @@ def merge_sort(array)
     end
     merge_sort(left_side)
     merge_sort(right_side)
+    merge(left_side, right_side, new_array)
   end
+  p new_array
+end
+
+def merge(arr1, arr2, arr_final)
+  a = 0
+  b = 0
+  c = 0
+  arr1_length = arr1.length
+  arr2_length = arr2.length
+  while a < arr1_length && b < arr2_length
+    if arr1[a] < arr2[b]
+      arr_final[c] = arr1[a]
+      a += 1
+      c += 1
+    elsif arr1[a] > arr2[b]
+      arr_final[c] = arr2[b]
+      b += 1
+      c += 1
+
+  end
+end
+return arr_final
 end
 
 
-array_numbers = [123, 232, 399, 345, 390, 855]
+array_numbers = [452, 874, 100, 345, 390, 855]
 merge_sort(array_numbers)

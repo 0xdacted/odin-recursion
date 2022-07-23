a3ed8fd990e18652dfdcdb40b867951f64654040
Red <crabmarket56@gmail.com>
2022-07-22 20:12:17 -0400

    Clean formatting

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,23 +1,23 @@
 def fibs(n)
-result = []
-i = 0
-  until i == n 
-    if i < 2
-      result[i] = i
-    else
-      result[i] = result[i-1] + result[i-2]
-    end
+  result = []
+  i = 0
+  until i == n
+    result[i] = if i < 2
+                  i
+                else
+                  result[i - 1] + result[i - 2]
+                end
     i += 1
   end
   p result
 end
 
 def fibs_rec(n, result = [])
-return [0,1] if n == 2
-result = fibs_rec(n -1)
-result << result[-1] + result[-2]
-p result
-end
+  return [0, 1] if n == 2
 
+  result = fibs_rec(n - 1)
+  result << result[-1] + result[-2]
+  p result
+end
 
 fibs(8)
--- merge_sort.rb
+++ merge_sort.rb
@@ -2,12 +2,12 @@ def merge_sort(array)
   new_array = []
   if array.length < 2
     return array
-
   else
-    left_side = merge_sort(array[0...array.length/2])
-    right_side = merge_sort(array[array.length/2...array.length])
+    left_side = merge_sort(array[0...array.length / 2])
+    right_side = merge_sort(array[array.length / 2...array.length])
     merge(left_side, right_side, new_array)
   end
+
   p new_array
 end
 
@@ -15,8 +15,6 @@ def merge(arr1, arr2, arr_final)
   a = 0
   b = 0
   c = 0
-  arr1_length = arr1.length
-  arr2_length = arr2.length
   while a < arr1_length && b < arr2_length
     if arr1[a] < arr2[b]
       arr_final[c] = arr1[a]
@@ -28,14 +26,13 @@ def merge(arr1, arr2, arr_final)
       c += 1
     end
   end
-  if a == arr1_length 
-    arr_final[c...arr1_length + arr2_length] = arr2[b...arr2_length]
-  elsif b == arr2_length 
-    arr_final[c...arr1_length + arr2_length] = arr1[a...arr1_length]
+  if a == arr1.length
+    arr_final[c...arr1.length + arr2.length] = arr2[b...arr2.length]
+  elsif b == arr2_length
+    arr_final[c...arr1.length + arr2.length] = arr1[a...arr1.length]
+  end
+  arr_final
 end
-return arr_final
-end
-
 
 array_numbers = [452, 874, 100, 345, 390, 855, 1000, 123]
 merge_sort(array_numbers)
